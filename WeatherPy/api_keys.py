21c88098a5f7f831f0e94cd02b6aca37df2c6f3f
Brook Miller <millerbrook@gmail.com>
2021-02-03 07:55:14 -0500

    Completes draft of WeatherPy

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,4 @@
 # OpenWeatherMap API Key
-weather_api_key = "f032ab5d1a49627ce525a435e61f79d4"
-
+weather_api_key = "8ce985fd3f5de9c99fe10a2f4b766105"
 # Google API Key
 g_key = "AIzaSyAjofc4ZLuKXnkpto9-ylcWl3NHPs_4rN8"
