68c73a6964c8c776e6bbe18e0d9f6abee8620a2a
Brook Miller <millerbrook@gmail.com>
2021-02-06 17:01:08 -0500

    Converts weather.py regressions to Fahrenheit and adjusts lin. reg. line locations

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
-# OpenWeatherMap API Key
-weather_api_key = "PUT KEY HERE"
+# Google Key
+g_key = "YOUR KEY HERE"
 
-# Google API Key
-g_key = "PUT KEY HERE"
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE"
