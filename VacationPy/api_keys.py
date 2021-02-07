6a2935ab79cea34bcfc67ce76c184ab775fddd5d
Brook Miller <millerbrook@gmail.com>
2021-02-06 21:48:11 -0500

    Adds screenshots, completes README and observations

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,6 +1,6 @@
 
 #Google Key
-g_key = "AIzaSyAjofc4ZLuKXnkpto9-ylcWl3NHPs_4rN8"
+g_key = "YOUR KEY HERE"
 
 # OpenWeatherMap API Key
-weather_api_key = "8ce985fd3f5de9c99fe10a2f4b766105"
+weather_api_key = "YOUR KEY HERE"
